6d32cb01bafd3c8b32d0a139a89dd50a0d5bf610
joseph walter <joeywalter@josephs-MacBook-Pro.local>
2018-10-11 09:41:57 -0600

    added a header and fetching the needed data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/header';
 
@@ -42,17 +41,13 @@ class App extends Component {
       <div className="App">
         <Header />
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+           
           </a>
         </header>
       </div>
--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <header className="Header-head">
         <Container id="Header-container">
         <Row>
-          <Col md="4"><img src={require("https://seeklogo.com/images/B/book-logo-2B8BEBAFCE-seeklogo.com.png")} id="Header-img" alt="Logo"/></Col>
+          <Col md="4"><img src={("http://seeklogo.com/images/B/book-logo-2B8BEBAFCE-seeklogo.com.png")} id="Header-img" alt="Logo"/></Col>
           <Col md="4"></Col>
           <Col md="4" id="Header-search"><input type="text" id="Header-input" placeholder="Search"/></Col>
         </Row>
